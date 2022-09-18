030f43d963c9ea73522be1e9bc6486c9221a2d1e
redtoorange <redtoorange@gmail.com>
2022-09-18 16:27:05 -0400

    Added level transitions

---
+++ Assets/Levels/TransitionController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace TowerCreep.Levels
+{
+    public class TransitionController : MonoBehaviour
+    {
+        [SerializeField] private float transitionTime;
+        [SerializeField] private RectTransform shroudCanvas;
+
+        private void Start()
+        {
+            PlayTransition();
+        }
+
+        public void PlayTransition()
+        {
+            shroudCanvas.gameObject.SetActive(true);
+            LeanTween.alpha(shroudCanvas, 0.0f, transitionTime).setOnComplete(() => shroudCanvas.gameObject.SetActive(false));
+        }
+    }
+}
